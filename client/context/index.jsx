7e988846852c177cc5da4bcd0a105c4c640e595c
Gopal Verma <vgopaldps@gmail.com>
2023-02-28 01:30:25 +0530

    tried to solve error

--- client/context/index.jsx
+++ client/context/index.jsx
@@ -5,24 +5,17 @@ import { ethers } from 'ethers';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-    const { contract } = useContract('0x6F42d7a993BCd27854ff6de9827e579397dc51F1')
+    const { contract } = useContract("0x6F42d7a993BCd27854ff6de9827e579397dc51F1");
     const address = useAddress();
     const connect = useMetamask();
-    const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
+    const { mutateAsync: createCampaign, isLoading } = useContractWrite(contract, "createCampaign");
+
     const publishCampaign = async (form) => {
         try {
-            const data = await createCampaign(
-                address,
-                form.title,
-                form.description,
-                form.category,
-                form.target,
-                new Date(form.deadline).getTime(),
-                form.image
-            )
-            console.log("Contract call success", data);
-        } catch (error) {
-            console.log("Contract call faileur", error);
+            const data = await createCampaign([address, "_title", "_description", "_category", 0.5, 23456123, "_image"]);
+            console.info("contract call successs", data);
+        } catch (err) {
+            console.error("contract call failure", err);
         }
     }
 
@@ -31,6 +24,7 @@ export const StateContextProvider = ({ children }) => {
             value={{
                 address,
                 contract,
+                connect,
                 createCampaign: publishCampaign,
             }}
         >
