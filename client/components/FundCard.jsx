74e2ac99d83cd5ff7fa1fbbe570f7b5d82451ea9
Gopal Verma <vgopaldps@gmail.com>
2023-03-17 00:23:59 +0530

    added campaignDetail Screen

--- client/components/FundCard.jsx
+++ client/components/FundCard.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-
 import { tagType, thirdweb } from '../assets';
 import { FaHashtag } from "react-icons/fa";
 import { RxCube } from "react-icons/rx";
 import { daysLeft } from '../utils';
 
-const FundCard = ({ owner, title, description, target, deadline, amountCollected, image, handleClick }) => {
+const FundCard = ({ owner, title, description, target, deadline, amountCollected, image }) => {
     const remainingDays = daysLeft(deadline);
     return (
-        <div className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer" onClick={handleClick}>
+        <div className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer">
             <img src={image} alt="fund" className="w-full h-[158px] object-cover rounded-[15px]" />
 
             <div className="flex flex-col p-4">
--- client/context/index.jsx
+++ client/context/index.jsx
@@ -48,6 +48,35 @@ export const StateContextProvider = ({ children }) => {
         return parsedCampaings;
     }
 
+    const getUserCampaigns = async () => {
+        const allCampaigns = await getCampaigns();
+
+        const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+
+        return filteredCampaigns;
+    }
+
+    const donate = async (pId, amount) => {
+        const data = await contract.call('donateToCampaign', pId, { value: ethers.utils.parseEther(amount) });
+
+        return data;
+    }
+
+    const getDonations = async (pId) => {
+        const donations = await contract.call('getDonators', pId);
+        const numberOfDonations = donations[0].length;
+
+        const parsedDonations = [];
+
+        for (let i = 0; i < numberOfDonations; i++) {
+            parsedDonations.push({
+                donator: donations[0][i],
+                donation: ethers.utils.formatEther(donations[1][i].toString())
+            })
+        }
+        return parsedDonations;
+    }
+
     return (
         <StateContext.Provider
             value={{
@@ -56,6 +85,9 @@ export const StateContextProvider = ({ children }) => {
                 connect,
                 createCampaign: publishCampaign,
                 getCampaigns,
+                getUserCampaigns,
+                donate,
+                getDonations
             }}
         >
             {children}
