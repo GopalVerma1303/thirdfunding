593b6ae1cb3518f5bbaea315e75506205cc82f1b
Gopal Verma <vgopaldps@gmail.com>
2023-03-20 02:14:24 +0530

    added profile section

--- client/pages/Profile.tsx
+++ client/pages/Profile.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useStateContext } from '../context'
+import DisplayCampaigns from '../components/DisplayCampaigns'
 
 function Profile() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [campaigns, setCampaigns] = useState([]);
+  const { address, contract, getCampaigns, getUserCampaigns } = useStateContext();
+  const fetchCampaigns = async () => {
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+    console.log(data);
+  }
+
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [address, contract]);
   return (
-    <div>Profile: A section that displays the user's profile information, including their name, profile picture, and history of contributions to projects.</div>
+    <DisplayCampaigns
+      title="Your Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
   )
 }
 
