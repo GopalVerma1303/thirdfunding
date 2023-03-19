82562c30983f1b6aa712cbfe5636e20d8f8aacb0
Gopal Verma <vgopaldps@gmail.com>
2023-03-19 13:26:28 +0530

    added landing pade

--- client/pages/CampaignDetails/[PostId].jsx
+++ client/pages/CampaignDetails/[PostId].jsx
@@ -104,9 +104,9 @@ function CampaignDetails() {
                         <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Creator</h4>
 
                         <div className="mt-[20px] flex flex-row items-center flex-wrap gap-[14px]">
-                            {/* <div className="w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#2c2f32] cursor-pointer">
-                                <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain" />
-                            </div> */}
+                            <div className="w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#2c2f32] cursor-pointer">
+                                <img src="/thirdweb.png" alt="user" className="w-[60%] h-[60%] object-contain" />
+                            </div>
                             <div>
                                 <h4 className="font-epilogue font-semibold text-[14px] text-white break-all">{campaign.owner}</h4>
                                 <p className="mt-[4px] font-epilogue font-normal text-[12px] text-[#808191]">10 Campaigns</p>
