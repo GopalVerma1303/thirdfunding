b855bcdae9279050379dad7340ca44d36ace95f3
Gopal Verma <vgopaldps@gmail.com>
2023-03-20 17:11:04 +0530

    random change

--- client/components/LandingPageComponents/HeroSection.tsx
+++ client/components/LandingPageComponents/HeroSection.tsx
@@ -18,7 +18,7 @@ function HeroSection() {
                 <BackgroundCircles />
                 <img src='/thirdfundinglogo_bg_removed.png' className='relative h-[100px] w-[100px] mx-auto object-cover' />
                 <h2 className=' text-[12px] sm:text-sm uppercase text-gray-500 sm:pb-10 pb-5 tracking-[15px]'>
-                    Welcome to
+                    WELCOME TO
                 </h2>
                 <h1 className='text-5xl lg:text-6xl sm:px-10 font-semibold'>
                     <span className='sm:text-[60px] text-[40px]  font-bold bg-gradient-to-r from-pink-500 to-purple-500 bg-clip-text text-transparent'>{text}</span>
