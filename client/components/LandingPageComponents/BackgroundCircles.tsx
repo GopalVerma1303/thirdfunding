c5a779e2c4f07f0af92889a440d83aaca8747a4e
Gopal Verma <vgopaldps@gmail.com>
2023-03-20 17:00:30 +0530

    downgraded ethers.js

--- client/components/LandingPageComponents/BackgroundCircles.tsx
+++ client/components/LandingPageComponents/BackgroundCircles.tsx
@@ -17,7 +17,7 @@ function BackgroundCircles() {
             }}
         >
             <div className='absolute border border-[#D30384] rounded-full h-[200px] w-[200px]  animate-ping mt-52' />
-            <div className='rounded-full border border-[#D30384] opacity-20  h-[300px] w-[300px] absolute mt-52' />
+            <div className='rounded-full border border-[#D30384] opacity-0  h-[300px] w-[300px] absolute mt-52' />
             <div className='rounded-full border border-[#D30384] opacity-20  h-[500px] w-[500px] absolute mt-52' />
             <div className='rounded-full border border-[#8c6dfd] opacity-20 h-[650px] w-[650px] absolute mt-52' />
             <div className='rounded-full border border-[#8c6dfd] opacity-20 h-[800px] w-[800px] absolute mt-52' />
