288a8d54034d30bf91b5eb69a5aacda456007e43
ugpec79 <ujjawalguptapec@gmail.com>
2023-03-26 11:27:21 +0530

    Backend of Chat ready

--- client/firebase/index.ts
+++ client/firebase/index.ts
@@ -4,7 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = { 
-    //add firebase keys here
+    apiKey: "AIzaSyB1f5FwbSRlF9caqDldqDUZOhq84twECN8",
+    authDomain: "thirdfunding.firebaseapp.com",
+    databaseURL: "https://thirdfunding-default-rtdb.firebaseio.com",
+    projectId: "thirdfunding",
+    storageBucket: "thirdfunding.appspot.com",
+    messagingSenderId: "144480579927",
+    appId: "1:144480579927:web:f07ee10321cf1f455bce12"
  
 };
 
