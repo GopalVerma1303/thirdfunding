4992ed7d7bdd5bfeb18df6791dd4daf1a4f45652
Ujjawal Gupta <113056503+ugpec79@users.noreply.github.com>
2023-03-29 15:31:12 +0530

    Update ConversationInbox.tsx

--- client/components/CommunityPageComponents/ConversationInbox.tsx
+++ client/components/CommunityPageComponents/ConversationInbox.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ConversationList from './ConversationList'
 import RoomList from './RoomList'
 
-function ConversationInbox() {
+function ConversationInbox(props:any) {
     return (
         <div className=' md:hidden absolute top-0 left-0 w-fit h-[89vh]  overflow-y-scroll scrollbar-none flex flex-row'>
          <RoomList userName={props.username} />
