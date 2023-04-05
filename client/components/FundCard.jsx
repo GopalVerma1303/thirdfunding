a38e7902b6b7f5572d5834ab8bb02294d242a581
Gopal Verma <vgopaldps@gmail.com>
2023-04-05 15:42:09 +0530

    made responsive setting w-[288px]

--- client/components/FundCard.jsx
+++ client/components/FundCard.jsx
@@ -4,17 +4,17 @@ import { FaHashtag } from "react-icons/fa";
 import { RxCube } from "react-icons/rx";
 import { daysLeft } from '../utils';
 
-const FundCard = ({ owner, title, description, category, target, deadline, amountCollected, image }) => {
+const FundCard = ({ owner, title, description, target, deadline, amountCollected, image }) => {
     const remainingDays = daysLeft(deadline);
     return (
-        <div className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer">
+        <div className="w-[288px] rounded-[15px] bg-[#1c1c24] cursor-pointer">
             <img src={image} alt="fund" className="w-full h-[158px] object-cover rounded-[15px]" />
 
             <div className="flex flex-col p-4">
                 <div className="flex flex-row items-center mb-[18px]">
                     {/* <img src={tagType} alt="tag" className="w-[17px] h-[17px] object-contain" /> */}
                     <FaHashtag className='text-[#808191]' />
-                    <p className="ml-[6px] mt-[2px] font-epilogue font-medium text-[12px] text-[#808191]">{category}</p>
+                    <p className="ml-[6px] mt-[2px] font-epilogue font-medium text-[12px] text-[#808191]">Education</p>
                 </div>
 
                 <div className="block">
